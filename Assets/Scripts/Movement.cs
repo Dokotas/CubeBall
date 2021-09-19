13912b1b936655ef531e2ef2aa8a152d933587fe
Dokotas <58700839+Dokotas@users.noreply.github.com>
2021-09-19 14:20:23 +0300

    init

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]private Camera _mainCamera;
+    
+    void Update()
+    {
+        if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            Debug.Log(Input.GetTouch(0).deltaPosition);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log( _mainCamera.ScreenToWorldPoint (Input.mousePosition));
+        }
+    }
+}
